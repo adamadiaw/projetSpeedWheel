295362003dc1c1d585ba387c0ba4e1d0fd518d66
adams <diawadama4@gmail.com>
2026-08-06 15:15:47 +0000

    fix(backend): force environment variables for DB and add port 8080 to setup_ports

--- setup_ports.bat
+++ setup_ports.bat
@@ -3,5 +3,6 @@ echo Configuration des redirections de ports Podman/WSL...
 netsh interface portproxy add v4tov4 listenport=8088 listenaddress=127.0.0.1 connectport=8088 connectaddress=172.25.45.33
 netsh interface portproxy add v4tov4 listenport=5432 listenaddress=127.0.0.1 connectport=5432 connectaddress=172.25.45.33
 netsh interface portproxy add v4tov4 listenport=6379 listenaddress=127.0.0.1 connectport=6379 connectaddress=172.25.45.33
+netsh interface portproxy add v4tov4 listenport=8080 listenaddress=127.0.0.1 connectport=8080 connectaddress=172.25.45.33
 echo Termine !
 pause
