69049987f2d678dc920215a0f1cb019cd87ba808
adams <diawadama4@gmail.com>
2026-08-07 20:02:25 +0000

    feat(docker): universal multi-stage Dockerfile for local and Jenkins

--- setup_ports.bat
+++ setup_ports.bat
@@ -4,5 +4,6 @@ netsh interface portproxy add v4tov4 listenport=8088 listenaddress=127.0.0.1 con
 netsh interface portproxy add v4tov4 listenport=5432 listenaddress=127.0.0.1 connectport=5432 connectaddress=172.25.45.33
 netsh interface portproxy add v4tov4 listenport=6379 listenaddress=127.0.0.1 connectport=6379 connectaddress=172.25.45.33
 netsh interface portproxy add v4tov4 listenport=8080 listenaddress=127.0.0.1 connectport=8080 connectaddress=172.25.45.33
+netsh interface portproxy add v4tov4 listenport=4200 listenaddress=127.0.0.1 connectport=4200 connectaddress=172.25.45.33
 echo Termine !
 pause
